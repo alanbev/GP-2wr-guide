0808f06beae837d7efd68c16cb17f27646cfee0d
alanbev <62112044+alanbev@users.noreply.github.com>
2022-02-23 23:45:00 +0000

    dep;oy version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react';
 import SymptomForm from './routes/SymptomForm.js';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
 import Report from './routes/report.js';
 import ColorectalForm from './routes/colorectalForm.js'
 import NonTwrForm from './routes/nonTWRref'
@@ -11,14 +11,14 @@ function App(props) {
     const [formData, setFormData]= useState();
 
     return(
-       <BrowserRouter>
+       <HashRouter>
     <Routes> {/* The Switch decides which component to show based on the current URL.*/}
-      <Route  exact path='/GP-2wr-aid' element={< SymptomForm setFormData={setFormData}/>}></Route>
-      <Route exact path='/GP-2wr-aid/Report' element={<Report formData={formData}/>}></Route>
-      <Route exact path='/GP-2wr-aid/colorectal' element={<ColorectalForm formData={formData}/>}></Route>
-      <Route exact path='/GP-2wr-aid/nonTWR' element={<NonTwrForm formData={formData}/>}></Route>
+      <Route  exact path='/' element={< SymptomForm setFormData={setFormData}/>}></Route>
+      <Route exact path='/Report' element={<Report formData={formData}/>}></Route>
+      <Route exact path='/colorectal' element={<ColorectalForm formData={formData}/>}></Route>
+      <Route exact path='//nonTWR' element={<NonTwrForm formData={formData}/>}></Route>
     </Routes>
-    </BrowserRouter>)
+    </HashRouter>)
 }
 
 export default App
